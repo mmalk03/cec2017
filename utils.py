e773ace23c9a97538f11b3301814eb9efc4e93a6
Piotr Podbielski <kontakt@piotrpodbielski.pl>
2020-06-30 15:17:58 +0200

    Add possibility to load results out of data/ directory which are not complete

---
+++ utils.py
@@ -0,0 +1,9 @@
+import argparse
+import os
+
+
+def dir_path(path):
+    if os.path.isdir(path):
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"readable_dir: {path} is not a valid path")
